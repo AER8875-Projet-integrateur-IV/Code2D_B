3d6bd43b1a985371b7bd8673e09c43b9e92a6e57
Alexandre Noël <alexandrenoel@pop-os.localdomain>
2020-11-19 16:36:26 -0500

    Small update solver

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -5,21 +5,27 @@ Solver::Solver(Mesh &mesh, Input &inputVals)
   :mesh_sol(mesh), input_sol(inputVals){
   // Initialize the first results
   Results Simulation = Results(mesh, inputVals);
-  //mesh_sol = mesh;
-  //input_sol = inputVals;
+  mesh_sol = mesh;
+  input_sol = inputVals;
 }
 
 // Compute the solver
 void Solver::ComputeSolver(){
   std::cout << "----- Starting iterative process -----" << '\n';
   Solver::UpdateBC();
+  for (int iElem = 0; iElem<mesh_sol.nElem; iElem++){
+
+  }
 }
 
 // Update boundary condition
 void Solver::UpdateBC(){
+  std::cout << "Boundary conditions update not done" << '\n';
   for (int iElem = 0; iElem<mesh_sol.nElem; iElem++){
     for (int iNfael = 0; iNfael<mesh_sol.nFael[iElem]; iNfael++){
-      
+      if (mesh_sol.eSuel[iElem][iNfael] > mesh_sol.nElem){
+
+      }
     }
   }
 }
