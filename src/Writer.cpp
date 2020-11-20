82a16afd70a9d759b644c0e9d0ca18eabf28e531
christian Brice kanmogne <christianbricekanmogne@yahoo.ca>
2020-11-19 22:54:42 -0500

    on proccess

--- src/Writer.cpp
+++ src/Writer.cpp
@@ -56,9 +56,9 @@ void Writer::writeNewZone(std::ofstream &filestream)
 {
 	filestream << "ZONE "
 	           << "ZONETYPE = FEQUADRILATERAL " 
-	           << "NODES = " << m_meshdata->nPoin() << ", "
-	           << "ELEMENTS = " << m_meshdata->nElem() << ", "
-	           << "FACES = " << m_meshdata->getnface() << ", "
+	           << "NODES = " << m_meshdata-> nPoin << ", "
+	           << "ELEMENTS = " << m_meshdata-> nElem << ", "
+	           << "FACES = " << m_meshdata-> getnface << ", "
 	           << "NUMCONNECTEDBOUNDARYFACES = 0, TOTALNUMBOUNDARYCONNECTIONS = 0\n "
 	           << "DATAPACKING = BLOCK, VARLOCATION = ([3-7] = CELLCENTERED) \n " ;
 }
